2b98054076719f70923c6945709f256e08ad1608
大橙子 <574445631@qq.com>
2025-03-16 08:49:46 +0800

    新增抽象handler

---
+++ src/main/java/org/bigorange/testai/handler/ChatAIHandler.java
@@ -0,0 +1,17 @@
+package org.bigorange.testai.handler;
+
+/**
+ * ai 对话处理器
+ */
+public abstract class ChatAIHandler {
+    String prompt = "   请基于以下HTML结构生成Selenium测试脚本：\n" +
+            "            - 语言要求：Java或Python\n" +
+            "            - 测试目标：验证主要功能按钮点击是否正常\n" +
+            "            - HTML内容:            %s";
+
+    public String generateTestScript(String html) {
+        return getScriptCode(getScriptCode(prompt + html));
+    }
+
+    public abstract String getScriptCode(String chat);
+}
---
+++ src/main/java/org/bigorange/testai/handler/DeepSeekHandler.java
@@ -0,0 +1,11 @@
+package org.bigorange.testai.handler;
+
+public class DeepSeekHandler extends ChatAIHandler{
+    //todo 实现DeepSeek 接口的请求
+
+    @Override
+    public String getScriptCode(String chat) {
+
+        return "";
+    }
+}
